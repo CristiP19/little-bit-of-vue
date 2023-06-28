05acf0e891782cb7178c130c94cc8c2a88218a71
CristiP19 <pogorcristian04@gmail.com>
2023-06-28 20:34:06 +0300

    Architecture and some code)

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div className="app">
+    <post-form @create="createPost"/>
+    <post-list :posts="posts"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PostForm from "@/components/PostForm";
+import PostList from "@/components/PostList";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    PostForm,
+    PostList,
+  },
+  data() {
+    return {
+      posts: [
+        {id: 1, title: "Javascript 1", body: "Description 1"},
+        {id: 2, title: "Javascript 2", body: "Description 2"},
+        {id: 3, title: "Javascript 3", body: "Description 3"},
+        {id: 4, title: "Javascript 4", body: "Description 4"},
+      ],
+    };
+  },
+  methods: {
+    createPost(post) {
+      this.posts.push(post);
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.app {
+  padding: 20px;
 }
 </style>
